f63962a45be087b26bc409f07347840c1602ca93
thanakamon <thanakamonmay@gmail.com>
2020-11-18 00:20:22 +0700

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,6 @@ var mongoose=require('mongoose');
 module.exports=()=>{
     mongoose.set('debug',true);
     var db=mongoose.connect('mongodb://localhost/s60030037');
-    require('../app/models/temp_rtc.model');
+    require('../app/models/temp.model');
     return db;
 }
